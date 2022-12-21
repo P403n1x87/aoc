955d76a8ab8f8cf3a765071960bcec826751e030
Gabriele Tornetta <gabriele.tornetta@datadoghq.com>
2022-12-21 17:13:59 +0000

    fix: 2022/19

--- 2022/19/code.py
+++ 2022/19/code.py
@@ -31,42 +31,52 @@ class Blueprint:
             )
 
         self.cost = tuple(self.cost)
-
         self.max_cost = tuple(max(cost[r] for cost in self.cost) for r in range(3))
-        print(self.cost)
+        self.max = 0
 
     @cache
     def geodes(self, t=24, rs=V(0, 0, 0, 0), bs=V(1, 0, 0, 0)):
-        if t <= 1:
-            return rs[-1] + bs[-1]
+        if t == 2:
+            v = rs[-1] + bs[-1] * 2 + all(r >= c for r, c in zip(rs, self.cost[-1]))
+            self.max = max(self.max, v)
+            return v
 
-        if any(b > c for b, c in zip(bs, self.max_cost)):
+        if rs[-1] + bs[-1] * t + t * (t - 1) / 2 <= self.max:
             return rs[-1]
-
-        # if any(rs[i] + bs[i] * (t - 1) > self.max_cost[i] * t for i in range(1)):
-        #     return rs[-1]
 
         if all(r >= c for r, c in zip(rs, self.cost[-1])):
             return self.geodes(t - 1, rs - self.cost[-1] + bs, bs + self.GEODE)
 
-        buildable = [all(r >= c for r, c in zip(rs, cs)) for cs in self.cost[:-1]]
-        return max(
-            rs[-1],
-            self.geodes(t - 1, rs + bs, bs),
-            *(
-                self.geodes(
-                    t - 1,
-                    rs - self.cost[r] + bs,
-                    bs + V.e(r, 4),
-                )
-                for r, b in enumerate(buildable)
-                if b
-            ),
+        buildable = [
+            all(r >= c for r, c in zip(rs, cs)) and b < m and r + b * t < m * t
+            for cs, m, r, b in zip(self.cost, self.max_cost, rs, bs)
+        ]
+        return (
+            max(
+                max(
+                    self.geodes(
+                        t - 1,
+                        rs - self.cost[r] + bs,
+                        bs + V.e(r, 4),
+                    )
+                    for r, b in list(enumerate(buildable))[::-1]
+                    if b
+                ),
+                self.geodes(t - 1, rs + bs, bs),
+            )
+            if any(buildable)
+            else self.geodes(t - 1, rs + bs, bs)
         )
 
 
+def po(arg):
+    i, bp = arg
+    return (i + 1) * Blueprint(bp).geodes(t=24)
+
+
 def part_one(data="input.txt"):
-    return sum((i + 1) * Blueprint(_).geodes(t=24) for i, _ in enumerate(gl(data)))
+    with Pool() as p:
+        return sum((p.map(po, enumerate(gl(data)))))
 
 
 def pt(bp):
@@ -74,9 +84,8 @@ def pt(bp):
 
 
 def part_two(data="input.txt"):
-    with Pool(3) as p:
+    with Pool() as p:
         return prod(p.map(pt, (bp for _, bp in zip(range(3), gl(data)))))
-    return prod(Blueprint(bp).geodes(t=32) for _, bp in zip(range(3), gl(data)))
 
 
 def test():
